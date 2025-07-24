73bb30ae93265a1ced8d8352b57a3fb714100f2f
tupo-nain <ueongos88z@gmail.com>
2025-07-25 00:04:31 +0300

    holy...

---
+++ h264flut_4ch.py
@@ -0,0 +1,167 @@
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GLib
+
+fallback_timeout = 5 #seconds!11!!!!
+
+#apply uall
+#\/DAT'S DEFAULT!11!1!!!!!
+video_width = 800
+video_height = 600
+listen_port = 5000
+listen_host = "0.0.0.0"
+server_listen_port = 5000
+server_listen_host = "0.0.0.0"
+nogui = False
+toptext_str = f"ONLY h264"
+bottomtext_str = f"No NSFW plz | running by CHANGEME and Gstreamer"
+novideotext_str = "NOVIDEO0)0))"
+toptext_font = "impact"
+bottomtext_font = "impact"
+novideotext_font = "arial"
+
+Gst.init(None)
+
+resize_cups = f"video/x-raw,width={video_width},height={video_height},pixel-aspect-ratio=(fraction)1/1"
+pipeline = Gst.Pipeline.new("h264flut")
+
+#spawn uall
+udp_ch1 = Gst.ElementFactory.make("udpsrc","udp_ch1")
+cfilt_ch1 = Gst.ElementFactory.make("capsfilter","cfilt_ch1")
+depay_ch1 = Gst.ElementFactory.make("rtph264depay","depay_ch1")
+que_ch1 = Gst.ElementFactory.make("queue","que_ch1")
+decodee_ch1 = Gst.ElementFactory.make("avdec_h264","decode_ch1")
+scale_ch1 = Gst.ElementFactory.make("videoscale","scale_ch1")
+rescale_ch1 = Gst.ElementFactory.make("capsfilter","rescale_ch1")
+fallback_ch1 = Gst.ElementFactory.make("fallbackswitch","fallback_ch1")
+imgfrz_ch1 = Gst.ElementFactory.make("imagefreeze","imgfrz_ch1")
+bottomtext_ch1 = Gst.ElementFactory.make("textoverlay","bottomtext_ch1")
+toptext = Gst.ElementFactory.make("textoverlay","toptext")
+pholder_ch1 = Gst.ElementFactory.make("videotestsrc","pholder_ch1")
+scale_phold_ch1 = Gst.ElementFactory.make("videoscale","scale_phold_ch1")
+res_phold_ch1 = Gst.ElementFactory.make("capsfilter","res_phold_ch1")
+
+if nogui:
+    mjpeg_encode = Gst.ElementFactory.make("avenc_mjpeg","mjpeg_encode")
+    output = Gst.ElementFactory.make("tcpserversink","GUI_output")
+else:
+    output = Gst.ElementFactory.make("autovideosink","GUI_output")
+
+#setup uall
+udp_src.set_property("port",listen_port)
+udp_src.set_property("uri",f"udp://{listen_host}:{listen_port}")
+
+input_queue.set_property("leaky",2)
+
+input_time_overlay.set_property("line-alignment",0)
+input_time_overlay.set_property("font-desc","arial")
+input_time_overlay.set_property("auto-resize",True)
+input_time_overlay.set_property("datetime-format","%t")
+
+rescale_input.set_property("caps",Gst.Caps.from_string(resize_cups))
+
+last_in_pic_frz.set_property("is-live",True)
+last_in_pic_frz.set_property("allow-replace",True)
+
+bottomtext.set_property("font-desc",bottomtext_font)
+bottomtext.set_property("text",bottomtext_str)
+bottomtext.set_property("auto-resize",False)
+bottomtext.set_property("halignment",5)
+bottomtext.set_property("valignment",5)
+bottomtext.set_property("xpos",0.5)
+bottomtext.set_property("ypos",0.98)
+
+toptext.set_property("font-desc",toptext_font)
+toptext.set_property("text",toptext_str)
+toptext.set_property("auto-resize",False)
+toptext.set_property("halignment",5)
+toptext.set_property("valignment",5)
+toptext.set_property("xpos",0.5)
+toptext.set_property("ypos",0.018)
+
+black_screen_src.set_property("pattern",2)
+black_screen_src.set_property("is-live",True)
+
+rescale_blank.set_property("caps",Gst.Caps.from_string(resize_cups))
+
+novideotext.set_property("font-desc",novideotext_font)
+novideotext.set_property("text",novideotext_str)
+novideotext.set_property("auto-resize",False)
+novideotext.set_property("halignment",5)
+novideotext.set_property("valignment",5)
+novideotext.set_property("xpos",0.5)
+novideotext.set_property("ypos",0.5)
+novideo_switch.set_property("immediate-fallback",True)
+novideo_switch.set_property("timeout",fallback_timeout * 1000000000)
+if nogui:
+    output.set_property("port",server_listen_port)
+    output.set_property("host",server_listen_host)
+
+pipeline.add(udp_src)
+pipeline.add(capsfilter)
+pipeline.add(rtp_depay)
+pipeline.add(input_queue)
+pipeline.add(h264_decode)
+pipeline.add(input_time_overlay)
+pipeline.add(prerescale_input)
+pipeline.add(rescale_input)
+pipeline.add(novideo_switch)
+pipeline.add(last_in_pic_frz)
+pipeline.add(bottomtext)
+pipeline.add(toptext)
+pipeline.add(black_screen_src)
+pipeline.add(prerescale_blank)
+pipeline.add(rescale_blank)
+pipeline.add(novideotext)
+if nogui:
+    pipeline.add(mjpeg_encode)
+pipeline.add(output)
+
+udp_src.link(capsfilter)
+capsfilter.link(rtp_depay)
+rtp_depay.link(input_queue)
+input_queue.link(h264_decode)
+h264_decode.link(prerescale_input)
+prerescale_input.link(rescale_input)
+rescale_input.link(input_time_overlay)
+input_time_overlay.link(novideo_switch)
+novideo_switch.link(last_in_pic_frz)
+last_in_pic_frz.link(toptext)
+toptext.link(bottomtext)
+
+#TOPOLOGY
+#udp_src > capfilter > rtp_depay > input_queue > h264_decode > 
+#> prerescale_input > rescale_input > input_time_overlay >
+#> novideo_switch > last_in_pic_frz > toptext >
+if nogui:
+    bottomtext.link(mjpeg_encode)
+    mjpeg_encode.link(output) # toptext > mjpeg_encode > tcpserversink
+else:
+    bottomtext.link(output) # toptext > autovideosink
+
+black_screen_src.link(prerescale_blank)
+prerescale_blank.link(rescale_blank)
+rescale_blank.link(novideotext)
+novideotext.link(novideo_switch)
+#ONE MORE TOPOLOGY
+#black_screen_src > prerescale_blank > rescale_blank > novideotext
+
+def handle_gstreamer_message(_bus: Gst.Bus, message: Gst.Message, loop: GLib.MainLoop):
+    message_type = message.type
+    if message_type == Gst.MessageType.ERROR:
+        err, debug = message.parse_error()
+        print(err, debug)
+
+try:
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    pipeline.set_state(Gst.State.PLAYING)
+    loop = GLib.MainLoop()
+    bus.connect('message', handle_gstreamer_message, loop)
+    loop.run()
+except KeyboardInterrupt:
+    print("\nStopping...")
+except GLib.Error as e:
+    print(e)
+finally:
+    pipeline.set_state(Gst.State.NULL)
